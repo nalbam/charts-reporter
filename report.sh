eb7e5cbb16e24185dc1efaab04c0bc5d7a4a00fc
nalbam <me@nalbam.com>
2018-09-17 10:35:35 +0900

    release version

--- report.sh
+++ report.sh
@@ -55,7 +55,23 @@ popd
 echo
 
 # release version
-VERSION=$(echo ${VERSION:-v0.0.0} | perl -pe 's/^(([v\d]+\.)*)(\d+)(.*)$/$1.($3+1).$4/e')
+if [ -z ${VERSION} ]; then
+    VERSION=$(cat ./VERSION | xargs)
+else
+    MAJOR=$(cat ./VERSION | xargs | cut -d'.' -f1)
+    MINOR=$(cat ./VERSION | xargs | cut -d'.' -f2)
+
+    LATEST_MAJOR=$(echo ${VERSION} | cut -d'.' -f1)
+    LATEST_MINOR=$(echo ${VERSION} | cut -d'.' -f2)
+
+    if [ "${MAJOR}" != "${LATEST_MAJOR}" ] || [ "${MINOR}" != "${LATEST_MINOR}" ]; then
+        VERSION=$(cat ./VERSION | xargs)
+    fi
+
+    # add
+    VERSION=$(echo ${VERSION} | perl -pe 's/^(([v\d]+\.)*)(\d+)(.*)$/$1.($3+1).$4/e')
+fi
+
 printf "${VERSION}" > target/VERSION
-echo ${VERSION}
+echo "VERSION=${VERSION}"
 echo
