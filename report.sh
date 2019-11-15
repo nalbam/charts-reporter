e5d4c94a61dd892ffae418c4f5e925b479ff1753
nalbam <me@nalbam.com>
2019-11-15 16:48:34 +0900

    helm 3.0

--- report.sh
+++ report.sh
@@ -14,49 +14,18 @@ mkdir -p ${SHELL_DIR}/target/previous
 mkdir -p ${SHELL_DIR}/target/versions
 mkdir -p ${SHELL_DIR}/target/release
 
-TMP=/tmp/charts
-
-_check_repo() {
-    CHART="$1"
-
-    REPO="$(echo $CHART | cut -d'/' -f1)"
-    NAME="$(echo $CHART | cut -d'/' -f2)"
-
-    if [ "${REPO}" == "incubator" ]; then
-        REPO_URL="https://storage.googleapis.com/kubernetes-charts-incubator"
-    elif [ "${REPO}" == "argo" ]; then
-        REPO_URL="https://argoproj.github.io/argo-helm"
-    elif [ "${REPO}" == "jetstack" ]; then
-        REPO_URL="https://charts.jetstack.io"
-    elif [ "${REPO}" == "harbor" ]; then
-        REPO_URL="https://helm.goharbor.io"
-    elif [ "${REPO}" == "monocular" ]; then
-        REPO_URL="https://helm.github.io/monocular"
-    elif [ "${REPO}" == "gitlab" ]; then
-        REPO_URL="https://charts.gitlab.io"
-    else
-        REPO_URL=""
-    fi
-
-    if [ "${REPO_URL}" != "" ]; then
-        COUNT=$(helm repo list | awk '{print $1}' | grep "${REPO}" | wc -l | xargs)
-
-        if [ "x${COUNT}" == "x0" ]; then
-            helm repo add ${REPO} ${REPO_URL}
-        fi
-    fi
-}
+TMP=/tmp/charts.json
 
 _check_version() {
     CHART="$1"
 
-    REPO="$(echo $CHART | cut -d'/' -f1)"
-    NAME="$(echo $CHART | cut -d'/' -f2)"
+    REPO="$(echo ${CHART} | cut -d'/' -f1)"
+    NAME="$(echo ${CHART} | cut -d'/' -f2)"
 
     touch ${SHELL_DIR}/target/previous/${NAME}
     NOW="$(cat ${SHELL_DIR}/target/previous/${NAME} | xargs)"
 
-    NEW="$(cat ${TMP} | grep "${CHART} " | head -1 | awk '{print $2" ("$3")"}' | xargs)"
+    NEW="$(cat ${TMP} | CHART="https://hub.helm.sh/charts/${CHART}" jq '[.[] | select(.url==env.CHART)][0] | "\(.version) \(.app_version)"' -r | awk '{print $1" ("$2")"}')"
 
     printf '# %-40s %-25s %-25s\n' "${CHART}" "${NOW}" "${NEW}"
 
@@ -95,21 +64,7 @@ if [ ! -z "${VERSION}" ]; then
     curl -sL https://github.com/${REPOSITORY}/releases/download/${VERSION}/versions.tar.gz | tar xz -C ${SHELL_DIR}/target/previous
 fi
 
-# helm init
-helm init --client-only
-echo
-
-# check repo
-while read VAR; do
-    _check_repo ${VAR}
-done < ${SHELL_DIR}/checklist.txt
-echo
-
-# repo update
-helm repo update
-echo
-
-helm search | awk '{print $1" "$2" "$3}' > ${TMP}
+helm search hub -o json > ${TMP}
 
 # check versions
 while read VAR; do
